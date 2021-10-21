63ac0a9fd0e0aa51045846453d037358d1c91372
hockypockyjh07 <jiahongkjh2000@gmail.com>
2021-10-21 21:56:22 +0800

    commit on counselling pages

---
+++ mymfrontend/src/counselling-pages/FAQ.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class FAQ extends React.Component {
+    render() {
+        return (
+            // content CSS are aligned to right
+            <section className="content">
+                <div className="container-fluid">
+                    <div>
+                        <h2> FAQ </h2>
+
+                        
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default FAQ
