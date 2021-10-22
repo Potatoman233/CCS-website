0277e7a8f8ba62443c5d714cb4bcf12c8c989e40
Isaac0211 <92616381+Isaac0211@users.noreply.github.com>
2021-10-22 22:03:15 +0800

    enter

--- mymfrontend/src/counselling-pages/HomePage.js
+++ mymfrontend/src/counselling-pages/HomePage.js
@@ -7,18 +7,23 @@ class HomePage extends React.Component {
             <section className="content">
                 <div className="container-fluid">
                     <div>
-                        <h2> You deserve to be understood. </h2> 
+                        <br />
+                        <h3> You deserve to be understood. </h3> 
+                        <br />
                         <p>
                         You are not being understood by anyone you know and we know you <br />
                         crave for being understood. We understand that you’re tired of talking <br />
                         about it with the people. But we want to understand you, truly. And we <br />
                         will listen to you without being jugmental. No third person.
                         </p>
-                        <h2>Just us.</h2>
+                        <h3>Just us.</h3>
 
                         <button className="btn btn-primary m-t-15">Share your story now</button>
 
+                        <br />
+                        <br />
                         <h3>8 Signs You Need Counselling</h3>
+                        <br />
                         <p>
                         You’re less productive and finding it difficult to focus <br />
                         You don’t feel excited about anything anymore <br />
@@ -28,7 +33,8 @@ class HomePage extends React.Component {
                         You’re having negative thoughts all the time <br />
                         You’re not as happy as you should be <br />
                         You feel like you’re on the brink of breaking down</p>
-                        <h4>Bear in mind, you don’t have to help yourself by yourself.</h4>
+                        <br />
+                        <h3>Bear in mind, you don’t have to help yourself by yourself.</h3>
      
 
                     </div>
