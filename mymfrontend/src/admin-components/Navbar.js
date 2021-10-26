e09284346dde4ed6820dbde88c35cb6c1952e257
TylerMotherFucking1 <lukealexandre.t@gmail.com>
2021-10-26 15:47:02 +0800

    Update Navbar.js

--- mymfrontend/src/admin-components/Navbar.js
+++ mymfrontend/src/admin-components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import user1 from 'adminbsb-materialdesign/images/user1.jpeg'
 class Navbar extends React.Component {
     render() {
         return (
             <nav className="navbar">
-                <div className="container-fluid">
+                <div className="container-fluid"> 
                     <div className="navbar-header">
-                        <a href="#" className="bars" onClick={this.props.onBarClick}></a>
-                        <a className="navbar-brand" href="#">Taylor's University Counselling Service</a>
-                    </div>
-
+                      <img src={user1} width="50" height="50" alt="Avatar" /> 
+                      <a href="#" className="bars" onClick={this.props.onBarClick}></a>    
+                    </div> 
+                    <a className="navbar-brand" href="#">Taylor's University Counselling Service</a>
+                    
                 </div>
             </nav>
         )
