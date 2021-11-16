5381f65ee76b6b84583a73f946dd8579a64ab20e
Isaac0211 <zhencong0211@gmail.com>
2021-11-16 13:15:41 +0800

    enter (updated events/home/team)

--- mymfrontend/src/counselling-pages/HomePage.js
+++ mymfrontend/src/counselling-pages/HomePage.js
@@ -30,29 +30,91 @@ class HomePage extends React.Component {
                         <h3>Talk to us if you go through these experiences.</h3>
                         <br />
                         <p>
-                        You’re less productive and finding it difficult to focus <br />
-                        You don’t feel excited about anything anymore <br />
-                        You’ve pulled away from your friends <br />
-                        You’re struggling in your relationship <br />
-                        You’re having sleeping difficulties <br />
-                        You’re having negative thoughts all the time <br />
-                        You’re not as happy as you should be <br />
-                        You feel like you’re on the brink of breaking down</p>
+                        <li>You’re less productive and finding it difficult to focus </li>
+                        <li>You don’t feel excited about anything anymore </li>
+                        <li>You’ve pulled away from your friends </li>
+                        <li>You’re struggling in your relationship </li>
+                        <li>You’re having sleeping difficulties </li>
+                        <li>You’re having negative thoughts all the time </li>
+                        <li>You’re not as happy as you should be </li>
+                        <li>You feel like you’re on the brink of breaking down </li>
+                        </p>
                         <br />
                         <h3>Our Services</h3>
                         <br />
+                        <h4>Individual Counselling</h4>
                         <br />
+                        <p>
+                        A process which allows clients to discuss their concern or worries, develop better <br />
+                        self-awareness and coping strategies with a counsellor in a one-on-one confidential <br />
+                        environment.
+                        </p>
                         <br />
+
+                        <h4>Group Counselling</h4>
+                        <br />
+                        <p>
+                        Comprises of a small group of individuals with similar concerns who, with a <br />
+                        counsellor to explore their issues together through group discussion and <br />
+                        interaction in a safe and confidential environment.    
+                        </p>
+                        <br />
+
+                        <h4>Psychological and Well-being Consultation</h4>
+                        <br />
+                        <p>
+                        Counselling aims to cover all areas of psychological and well-being services <br />
+                        through personal discussions with students, parents and staff.
+                        </p>
+                        <br />
+
+                        <h4>Student Special Needs Support</h4>
+                        <br />
+                        <p>
+                        A support system provided to assist students with special needs who are medically <br />
+                        or clinically diagnosed.    
+                        </p>
+                        <br />
+
+                        <h4>Crisis Interventions</h4>
+                        <br />
+                        <p>
+                        An immediate intervention taken by the counsellors to assist in the care of those in <br />
+                        crisis situation which may include an initiation of counselling or a referral to <br />
+                        additional university and community resources.
+                        </p>
+                        <br />
+
+                        <h4>Assistance with Referrals to Community Resources</h4>
+                        <br />
+                        <p>
+                        A support system for referral of clients requiring additional external resources <br />
+                        extending beyond the limits of counselling services. 
+                        </p>
                         <br />
                         <h3>Our Team</h3>
                         <br />
-                        <br />
-                        <br />
+                        <ol>
+                            <li>Ng Shan Na - Head of CCS</li>
+                            <li>Kok Choon Foong (Evan) - Senior Counsellor</li>
+                            <li>Nithya Ramasamy - Counsellor</li>
+                            <li>Noorkarmila Hamid - Counsellor</li>
+                            <li>Esther Chu Yun Sing - Counsellor</li>
+                        </ol>
                         <br />
                         <h3>Testimonials</h3>
                         <br />
+                        <p>
+                        "I was in quite a bad place when I first came here. However, over the last year, the staff <br />
+                        here have been very kind and now I am going back home feeling so much better.<br />
+                        </p>
                         <br />
-                        <br />
+                        <p>
+                        I am very thankful for the attention the staff gave to me, and how keen they were with <br />
+                        following up on me. Thanks to this department I am more accepting of my issues now <br />
+                        and I am better able to understand and accept the fact that we all have problems, big <br />
+                        and small and that there is no harm in asking for help."
+                        </p>
                         <br />
                         <h3>“We are ready to listen to you.”</h3>
                         <br />
--- mymfrontend/src/counselling-pages/Team.js
+++ mymfrontend/src/counselling-pages/Team.js
@@ -8,6 +8,14 @@ class Team extends React.Component {
                 <div className="container-fluid">
                     <div>
                         <h2> Team </h2>
+                        <br />
+                        <ol>
+                            <li>Ng Shan Na - Head of CCS</li>
+                            <li>Kok Choon Foong (Evan) - Senior Counsellor</li>
+                            <li>Nithya Ramasamy - Counsellor</li>
+                            <li>Noorkarmila Hamid - Counsellor</li>
+                            <li>Esther Chu Yun Sing - Counsellor</li>
+                        </ol>
 
                         
                     </div>
